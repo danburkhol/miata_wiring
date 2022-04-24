b47f1cf9841bae29a7b0f415d636bde96fe3c5c3
Daniel Burkholder <danburkhol@gmail.com>
2022-04-24 08:52:16 -0400

    ecu power

--- makeviz.sh
+++ makeviz.sh
@@ -3,25 +3,26 @@
 # https://github.com/formatc1702/WireViz
 # https://mikefarah.gitbook.io/yq/operators/multiply-merge
 
-# generic
-yq '. *= load("src/connectors.yml")' src/connectors_efi.yml > generated/connectors_efi.yml
-
 # injectors
-yq '. *= load("generated/connectors_efi.yml")'  src/injectors.yml > generated/injectors.yml
+yq '. *= load("src/connectors.yml")'  src/injectors.yml > generated/injectors.yml
 wireviz generated/injectors.yml
 
 # ignition
-yq '. *= load("generated/connectors_efi.yml")'  src/ignition.yml > generated/ignition.yml
+yq '. *= load("src/connectors.yml")'  src/ignition.yml > generated/ignition.yml
 wireviz generated/ignition.yml
 
 # throttle
-yq '. *= load("generated/connectors_efi.yml")'  src/throttle.yml > generated/throttle.yml
+yq '. *= load("src/connectors.yml")'  src/throttle.yml > generated/throttle.yml
 wireviz generated/throttle.yml
 
 # knock
-yq '. *= load("generated/connectors_efi.yml")'  src/knock.yml > generated/knock.yml
+yq '. *= load("src/connectors.yml")'  src/knock.yml > generated/knock.yml
 wireviz generated/knock.yml
 
 # lsu
-yq '. *= load("generated/connectors_efi.yml")'  src/lsu.yml > generated/lsu.yml
+yq '. *= load("src/connectors.yml")'  src/lsu.yml > generated/lsu.yml
 wireviz generated/lsu.yml
+
+# ecu
+yq '. *= load("src/connectors.yml")'  src/ecu.yml > generated/ecu.yml
+wireviz generated/ecu.yml
