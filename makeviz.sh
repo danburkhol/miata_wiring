cff81db87d8ff986aeeb80dad45d63c0d593bb3e
Daniel Burkholder <danburkhol@gmail.com>
2022-04-24 08:24:02 -0400

    ecu to bulkhead for knock

--- makeviz.sh
+++ makeviz.sh
@@ -3,36 +3,21 @@
 # https://github.com/formatc1702/WireViz
 # https://mikefarah.gitbook.io/yq/operators/multiply-merge
 
-# yq eval-all '. as $item ireduce ({}; . * $item )' *.yml > generated/merged.yml
-# wireviz generated/merged.yml
-
-# yaml-merge src/connectors.yml src/ecu_to_bulkhead.yml > generated/ecu_to_bulkhead.yml
-# wireviz generated/ecu_to_bulkhead.yml
-
-# yaml-merge src/connectors.yml src/injectors.yml > generated/injectors.yml
-# wireviz generated/injectors.yml
-
-# rm generated/merged2.yml
-# cd src/
-# yq eval-all '. as $item ireduce ({}; . * $item )' *.yml > ../../generated/merged2.yml
-# cd ../..
-# wireviz generated/merged2.yml
+# generic
+yq '. *= load("src/connectors.yml")' src/connectors_efi.yml > generated/connectors_efi.yml
 
 # injectors
-yq '. *= load("src/connectors.yml")' src/connectors_efi.yml > generated/connectors_efi.yml
 yq '. *= load("generated/connectors_efi.yml")'  src/injectors.yml > generated/injectors.yml
 wireviz generated/injectors.yml
 
 # ignition
-yq '. *= load("src/connectors.yml")' src/connectors_efi.yml > generated/connectors_efi.yml
 yq '. *= load("generated/connectors_efi.yml")'  src/ignition.yml > generated/ignition.yml
 wireviz generated/ignition.yml
 
-yq '. *= load("src/connectors.yml")' src/connectors_efi.yml > generated/connectors_efi.yml
+# throttle
 yq '. *= load("generated/connectors_efi.yml")'  src/throttle.yml > generated/throttle.yml
 wireviz generated/throttle.yml
 
-# wireviz -o generated/injectors/ generated/injectors.yml
-# wireviz -o generated/injectors \
-# --prepend-file generated/connectors_efi.yml \
-# src/injectors.yml
+# knock
+yq '. *= load("generated/connectors_efi.yml")'  src/knock.yml > generated/knock.yml
+wireviz generated/knock.yml
