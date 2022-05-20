a94a4e2da2f87805c1fddfe015f265fe9eff5fb1
Daniel Burkholder <danburkhol@gmail.com>
2022-05-20 18:53:09 -0400

    strip anchors and re-instate them after merging to avoid yaml validation that is not relevent once all files are merged

--- makeviz.sh
+++ makeviz.sh
@@ -23,14 +23,20 @@ export_master () {
         # make a copy of the source file to inject common items into
         tmp_file_path=tmp/$(basename $src_file)
 
-        cp $src_file $tmp_file_path
+        sed -r 's/\*/42069/g' $src_file > $tmp_file_path
+        # cp $src_file $tmp_file_path
 
         # merges the content of src/common/connector_templates.yml into $tmp_file_path
         sed -i -e '/connectors:/{r src/common/connector_templates.yml' -e 'd}' $tmp_file_path
         # sed -i -e '/cables:/{r src/common/cable_templates.yml' -e 'd}' $tmp_file_path
     done
 
-    yq eval-all '. as $item ireduce ({}; . *+ $item )' tmp/*.yml > generated/master.yml
+    sed -ri 's/\*/42069/g' src/common/cable_templates.yml
+    cp src/common/cable_templates.yml generated/master.yml
+
+    yq eval-all '. as $item ireduce ({}; . *+ $item )' tmp/*.yml >> generated/master.yml
+    sed -ri 's/42069/\*/g' generated/master.yml
+
     wireviz generated/master.yml
 }
 
