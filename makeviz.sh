dd76b504e659cfbb1264339813d38998942d3c5f
Daniel Burkholder <danburkhol@gmail.com>
2022-07-21 19:13:15 -0400

    refactor to reduce copy paste

--- makeviz.sh
+++ makeviz.sh
@@ -56,21 +56,13 @@ export_master () {
         # make a copy of the source file to inject common items into
         tmp_file_path=tmp/$(basename $src_file)
 
-        sed -r 's/\*/42069/g' $src_file > $tmp_file_path
-        echo -e '\n' >> $tmp_file_path
-        # cp $src_file $tmp_file_path
-
-        # merges the content of src/templates/connector_templates.yml into $tmp_file_path
-        # sed -i -e '/connectors:/{r src/templates/connector_templates.yml' -e 'd}' $tmp_file_path
-        # sed -i -e '/cables:/{r src/templates/cable_templates.yml' -e 'd}' $tmp_file_path
+        strip_anchors $src_file $tmp_file_path
     done
 
-    sed -ri 's/\*/42069/g' src/templates/cable_templates.yml
-    cp src/templates/cable_templates.yml generated/master.yml
-    echo -e '\n' >> generated/master.yml
+    strip_anchors src/templates/cable_templates.yml generated/master.yml
 
     yq eval-all '. as $item ireduce ({}; . *+ $item )' tmp/*.yml >> generated/master.yml
-    sed -ri 's/42069/\*/g' generated/master.yml
+    restore_anchors generated/master.yml
 
     wireviz generated/master.yml
 
