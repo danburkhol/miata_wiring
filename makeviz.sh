8709b5973cf8c755f7d54d42cd43d71d73a30ca9
Daniel Burkholder <danburkhol@gmail.com>
2022-07-21 19:11:20 -0400

    method to output a single file

--- makeviz.sh
+++ makeviz.sh
@@ -18,6 +18,39 @@ cleanup() {
     rm -r tmp/ &> /dev/null
 }
 
+strip_anchors() {
+    # Remove asterisk from anchors to make yaml merging easier
+    # Store it in the given dir $2 with a new line
+    sed -r 's/\*/42069/g' $1 > $2
+    echo -e '\n' >> $2
+}
+
+restore_anchors() {
+    sed -ri 's/42069/\*/g' $1
+}
+
+export_file() {
+    src_file=$1
+    generated_file=$2
+
+    tmp_file_path=tmp/$(basename $src_file)
+
+    strip_anchors $src_file $tmp_file_path
+
+    # Seed the generated file
+    strip_anchors src/templates/cable_templates.yml $generated_file
+
+    # merge the src file into the templated file
+    yq eval-all '. as $item ireduce ({}; . *+ $item )' tmp/*.yml >> $generated_file
+
+    restore_anchors $generated_file
+
+    wireviz $generated_file
+
+    cleanup
+}
+
+
 export_master () {
     for src_file in src/*.yml; do
         # make a copy of the source file to inject common items into
@@ -50,3 +83,5 @@ export_master () {
 setup
 export_master
 cleanup
+
+export_file src/steering_column.yml generated/steering_column.yml
