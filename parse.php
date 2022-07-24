a10ba1e955cd1e12c076adb7c6c8e13643414c20
Daniel Burkholder <danburkhol@gmail.com>
2022-07-24 11:02:40 -0400

    method to export a master file

--- parse.php
+++ parse.php
@@ -55,25 +55,78 @@ function export_single_file($input_file, $common_connectors = [])
     );
 }
 
-function get_common_connectors()
+/**
+ * Concatonate an array of files into a single body and parse that as a yaml file
+ * Order of $files matters, each subsequent file is appended to the last
+ * @param array(string) $files
+ * @return array
+ */
+function concat_and_parse_yaml_files($files)
 {
-    $template_file          = ('./src/templates/cable_templates.yml');
-    $common_connectors_file = ('./src/common/connectors.yml');
-
-    return Yaml::parse(file_get_contents($template_file) . "\n" . file_get_contents($common_connectors_file));
+    return Yaml::parse(
+        array_reduce($files, function($out, $file) {
+            return (
+                $out."\n".file_get_contents($file)
+                // file_get_contents($file)."\n".$out
+            );
+        }, '')
+    );
 }
 
-function export_wireviz($file_path)
+function get_common_connectors()
+{
+    return concat_and_parse_yaml_files([
+        './src/templates/cable_templates.yml',
+        './src/common/connectors.yml',
+    ]);
+}
+
+/**
+ * Generate a global config of all source and template files merged together
+ * can be exported as-is or used to fill in gaps of other source files for individual export
+ * @return array
+ */
+function generate_global_config()
+{
+    $template_file     = ('./src/templates/cable_templates.yml');
+    $common_connectors = get_common_connectors();
+
+    return array_reduce(
+        glob('./src/*.yml'),
+        function($out, $file_path) use ($template_file, $common_connectors) {
+            $tmp = concat_and_parse_yaml_files([
+                $template_file,
+                $file_path,
+            ]);
+
+            foreach (['connectors', 'cables', 'connections'] as $key) {
+                $out[$key] = array_merge(
+                    ($tmp[$key] ?? []),
+                    ($common_connectors[$key] ?? []),
+                    ($out[$key] ?? [])
+                );
+            }
+            return $out;
+        },
+        []
+    );
+}
+
+function export_wireviz($file_path, $dest = null)
 {
     if (!file_exists('./tmp')) mkdir('./tmp');
 
-    $common_connectors = get_common_connectors();
-
-    export_single_file($file_path, $common_connectors);
     exec('wireviz ./tmp/'.basename($file_path));
 }
 
-export_wireviz('./src/power_windows.yml');
-export_wireviz('./src/wiper.yml');
-export_wireviz('./src/dashboard.yml');
-export_wireviz('./src/pedal.yml');
+function export_master($tmp_file_path = './tmp/master.yml')
+{
+    if (!file_exists('./tmp')) mkdir('./tmp');
+    $global_config = generate_global_config();
+
+    file_put_contents($tmp_file_path, Yaml::dump($global_config));
+
+    export_wireviz($tmp_file_path);
+}
+
+export_master();
