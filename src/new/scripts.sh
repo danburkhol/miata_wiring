553ec842326c8306926a2b8cfd4cd8ee2e1ea214
Daniel Burkholder <danburkhol@gmail.com>
2023-07-13 14:43:45 -0400

    rename

--- src/new/scripts.sh
+++ src/new/scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wireviz connectors.yml --prepend-file cables.yml
+wireviz interior_connectors.yml --prepend-file interior_cables.yml
 wireviz engine_bay.yml --prepend-file engine_bay_connections.yml
 
 # delete .gv, bom.tsv, and .png files
